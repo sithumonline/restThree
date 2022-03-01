d22d695ff5d6ec1966acca828f2b4c4caad1cc5b
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-03-02 01:10:10 +0530

    Implemented 3 sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [],
-}
+};
