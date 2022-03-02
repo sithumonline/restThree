4a7ad8153b2d48720e8fc75a6209280687aa9e25
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-03-03 01:40:23 +0530

    Add responsiveness

--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const Navbar = () => {
 
-    const [ theme, setTheme ] = useState(("theme" in localStorage) ? localStorage.theme : "dark");
+    const [theme, setTheme] = useState(("theme" in localStorage) ? localStorage.theme : "dark");
 
     useEffect(() => {
         if (!("theme" in localStorage)) {
@@ -34,18 +34,22 @@ const Navbar = () => {
     }
 
     return (
-        <div className="relative bottom-0 flex justify-center dark:text-white z-10">
+        <div className="relative flex justify-center dark:text-white z-10">
             {/* <div className="fixed bottom-8 rounded-full flex justify-center items-center backdrop-blur w-16 h-16 bg-slate-200/30 border border-slate-900/20">
                 <FontAwesomeIcon icon={faBars} />
             </div> */}
-            <div className="fixed bottom-8 rounded-lg flex gap-8 px-8 items-center backdrop-blur h-16 font-semibold text-lg bg-slate-200/60 border border-slate-900/10 dark:bg-neutral-900/70">
-                <a className="hover:text-orange-500" href="#home">Home</a>
-                <a className="hover:text-orange-500" href="#workreel">Work Reel</a>
-                <a className="hover:text-orange-500" href="#contact">Contact</a>
+            <div className="fixed top-0 w-full flex justify-between gap-8 px-8 sm:px-16 items-center backdrop-blur-lg h-16 font-semibold text-md bg-slate-200/70 border border-slate-900/10 dark:bg-neutral-900/80">
+                <div className="flex gap-10">
+                    <a className="hover:text-orange-500" href="#home">Home</a>
+                    <a className="hover:text-orange-500" href="#workreel">Work</a>
+                    <a className="hover:text-orange-500" href="#contact">Contact</a>
+                </div>
+                <div>
                 <button onClick={toggleTheme} className="font-semibold flex items-center gap-1 hover:text-orange-500">
-                    <FontAwesomeIcon icon={theme === "dark" ? faSun : faMoon} />
-                    {theme === "dark" ? "Light" : "Dark"}
+                    <FontAwesomeIcon icon={theme === "dark" ? faMoon : faSun} />
+                    <span className="hidden sm:block">{theme === "dark" ? "Dark" : "Light"}</span>
                 </button>
+                </div>
             </div>
         </div>
     );
